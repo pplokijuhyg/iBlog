7fa4830262addf204dc39b65e704791a15484f14
luyuan <1162963624@qq.com>
2020-02-23 13:55:51 +0800

    报错解决

--- server/api.ts
+++ server/api.ts
@@ -9,13 +9,13 @@ const app = express();
 
 app.use(cookieParser());
 app.use(bodyParser.json());
-app.all('*',function (req, res, next) {
+app.all('*', function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
   res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
 
-  if (req.method == 'OPTIONS') {
-    res.send(200); /让options请求快速返回/
+  if (req.method === 'OPTIONS') {
+    res.send(200); 
   }
   else {
     next();
