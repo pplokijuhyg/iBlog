6f0a72a1c3ee79a07afeb31488aee94a9c001ee3
luyuan <1162963624@qq.com>
2020-02-23 14:08:18 +0800

    语法？？？

--- server/api.ts
+++ server/api.ts
@@ -15,7 +15,7 @@ app.all('*', function (req, res, next) {
   res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
   if (req.method === 'OPTIONS') {
     res.send(200);
-  }else {
+  } else {
     next();
   }
 });
